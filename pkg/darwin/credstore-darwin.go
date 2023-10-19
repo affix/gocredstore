c0844f27ca345b1e2c4ce28bbb69d2f5f58cfee4
Keiran Smith <contact@keiran.scot>
2023-10-19 20:43:03 +0100

    Darwin stuff

--- pkg/darwin/credstore-darwin.go
+++ pkg/darwin/credstore-darwin.go
@@ -10,7 +10,7 @@ type DarwinCredStore struct{}
 // Write stores an item in the Darwin credential store with the given itemName and itemValue.
 // It returns an error if the item cannot be stored.
 func (d *DarwinCredStore) Write(itemName string, itemValue []byte) error {
-	item := keychain.NewGenericPassword(itemName, "gocredstore", "", itemValue, "")
+	item := buildItem(itemName)
 	item.SetSynchronizable(keychain.SynchronizableNo)
 	item.SetAccessible(keychain.AccessibleWhenUnlocked)
 	err := keychain.AddItem(item)
@@ -24,16 +24,31 @@ func (d *DarwinCredStore) Write(itemName string, itemValue []byte) error {
 // Read retrieves an item from the Darwin credential store with the given itemName.
 // It returns an error if the item cannot be found.
 func (d *DarwinCredStore) Delete(itemName string) error {
-	return keychain.DeleteGenericPasswordItem(itemName)
+	item := buildItem(itemName)
+	return keychain.DeleteItem(item)
 }
 
 // Delete removes the specified item from the Darwin keychain.
 // Delete removes an item from the Darwin credential store with the given itemName.
 // It returns an error if the item cannot be found or deleted.
 func (d *DarwinCredStore) Read(itemName string) ([]byte, error) {
-	item, err := keychain.GetGenericPassword(itemName, "gocredstore", "", "")
+	query := buildItem(itemName)
+	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetReturnData(true)
+	results, err := keychain.QueryItem(query)
 	if err != nil {
 		return nil, err
 	}
-	return item.Data, nil
+	password := string(results[0].Data)
+
+	return password, err
+}
+
+func buildItem(itemName string) keychain.Item {
+	item := keychain.NewItem()
+	item.SetSecClass(keychain.SecClassGenericPassword)
+	item.SetService("gocredstore")
+	item.SetAccount(itemName)
+	item.SetLabel(itemName)
+	return item
 }
